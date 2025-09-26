57c79466e377d57675f74ba40b29d381f3119256
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-27 02:32:43 +0530

    variable

--- roboshop.sh
+++ roboshop.sh
@@ -1,18 +1,20 @@
 #!/bin/bash 
 
+
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0e11bbf53d62f5bfb" # replace with your SG ID
+SG_ID="sg-07c8acf3fa6b923fa" # replace with your SG ID
+ZONE_ID="Z0948150OFPSYTNVYZOY" # replace with your ID
+DOMAIN_NAME="daws86s.fun"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
-   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    # Get private IP
-   if [ $instance != "frontend" ]; then
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-   else
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+    else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-   fi 
-
-   echo "$instance: $IP"
-done
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+    fi
